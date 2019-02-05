b4ab326d62e09c14d53762c4b3887f842c347534
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2019-02-05 22:28:30 +0100

    Add a parsing tool to validate JSON

---
+++ validate-nasawakeupcalls.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import json
+
+
+def pretty_json(dict_data):
+    return json.dumps(
+        dict_data, sort_keys=True, indent=4, separators=(',', ': '))
+
+
+def main():
+    """Primary entry point of the script."""
+    data = None
+    with open('nasawakeupcalls.json') as f:
+        data = json.load(f)
+    print(pretty_json(data))
+
+
+if __name__ == "__main__":
+    main()
